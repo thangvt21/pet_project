b475d52d7cfdf0c4bc13bdc49004bb07b84795fe
Thang Vo <thanhthang3375@gmail.com>
2024-10-29 09:23:02 +0700

    [update]: detect_barcode.py to detech PDF and images

--- projects/new_get_tracking.py
+++ projects/new_get_tracking.py
@@ -15,22 +15,13 @@ BASE_DIR = Path(__file__).resolve().parent
 SHEET_ID = "1lX8xs3zJVinhRs_r4itv6V8gAw4Aut8rY_waj3LHql4"
 JSON_PATH = BASE_DIR.parent / "luminous-lodge-321503-2defcccdcd2d.json"
 IMAGE_PATH = BASE_DIR.parent / "img"
-TESSERACT_PATH = BASE_DIR.parent / "Tesseract-OCR" / "tesseract.exe"
-
-# Regex of USPS tracking numbers
-USPS_22 = r"\d{4} \d{4} \d{4} \d{4} \d{4} \d{2}"
-USPS_26 = r"\d{4} \d{4} \d{4} \d{4} \d{4} \d{4} \d{2}"
-
-# Regex of FEDEX tracking numbers
-FEDEX2 = r"\bTRK# \d{4} \d{4} \d{4}\b"
-FEDEX1 = r"\bID# \d{4} \d{4} \d{4}\b"
-
-PATTERN_LIST = [FEDEX1, FEDEX2, USPS_22, USPS_26]
 
 
 def detect_link_and_transform(url: str):
     download_link = ""
-    ggdrive_link_pattern = r"(https?://drive\.google\.com/(?:file/d/|open\?id=)([a-zA-Z0-9_-]+))"
+    ggdrive_link_pattern = (
+        r"(https?://drive\.google\.com/(?:file/d/|open\?id=)([a-zA-Z0-9_-]+))"
+    )
     matches = re.findall(ggdrive_link_pattern, url)
     if not matches:
         return url
@@ -72,23 +63,31 @@ def get_worksheet_from_ggsheet(sheet_id: str, file_json: str):
     return worksheet
 
 
-def ocr_pdf_and_match(filepath: str):
-    result = ""
-    images = convert_from_path(filepath)
-    images = [np.array(i)[:, :, ::-1] for i in images]
-    for nr, image in enumerate(images):
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+def detect_barcode(filepath: str):
+    gray = ""
+    if filepath.endswith(".pdf"):
+        images = convert_from_path(filepath)
+        images = [np.array(i)[:, :, ::-1] for i in images]
+        for nr, image in enumerate(images):
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    else:
+        images = cv2.imread(filepath)
+        gray = cv2.cvtColor(images, cv2.COLOR_BGR2GRAY)
+    barcode = decode(gray)
+    data_barcode = barcode[0].data
+    result = re.search(r"(?<=\x1d)\d+", data_barcode.decode())
 
-        barcode = decode(gray)
-        data_barcode = barcode[0].data
-        result = re.search(r"(?<=\x1d)\d+", data_barcode.decode())
     return result.group()
 
 
 def main():
     worksheet = get_worksheet_from_ggsheet(SHEET_ID, str(JSON_PATH))
-    worksheet_data = worksheet.get_all_values(returnas="matrix", majdim="rows", include_tailing_empty=False,
-                                              include_tailing_empty_rows=False, )
+    worksheet_data = worksheet.get_all_values(
+        returnas="matrix",
+        majdim="rows",
+        include_tailing_empty=False,
+        include_tailing_empty_rows=False,
+    )
     for i, data_sheet in enumerate(worksheet_data, start=1):
         if data_sheet:
             url = detect_link_and_transform(data_sheet[3])
@@ -96,22 +95,39 @@ def main():
                 if url:
                     with requests.get(url, stream=True) as response:
                         response.raise_for_status()
-                        filename = get_filename_from_headers(response) or get_filename_from_url(url)
+                        filename = get_filename_from_headers(
+                            response
+                        ) or get_filename_from_url(url)
                         filepath = f"{IMAGE_PATH}/{filename}"
                         with open(filepath, "wb") as f:
                             for chunk in response.iter_content(chunk_size=8192):
                                 f.write(chunk)
-                    data = ocr_pdf_and_match(filepath)
-                    value = (str(data).replace(" ", "").replace("TRK#", "").replace("ID#", ""))
+                    data = detect_barcode(filepath)
+                    value = (
+                        str(data)
+                        .replace(" ", "")
+                        .replace("TRK#", "")
+                        .replace("ID#", "")
+                    )
                     worksheet.update_value(f"E{i}", value, parse=True)
                 else:
                     value = "Lỗi link label"
                     worksheet.update_value(f"E{i}", value, parse=True)
                     # Update the cell's background color
-                    worksheet.apply_format(f"E{i}:E{i}", {"backgroundColor": {"red": 1, "green": 0, "blue": 0}}, )
+                    worksheet.apply_format(
+                        f"E{i}:E{i}",
+                        {"backgroundColor": {"red": 1, "green": 0, "blue": 0}},
+                    )
                     continue
             except Exception as e:
                 print("Skipping empty link", e)
+                value = "Lỗi link label"
+                worksheet.update_value(f"E{i}", value, parse=True)
+                # Update the cell's background color
+                worksheet.apply_format(
+                    f"E{i}:E{i}",
+                    {"backgroundColor": {"red": 1, "green": 0, "blue": 0}},
+                )
 
 
 main()
