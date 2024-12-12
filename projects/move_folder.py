78a3456668cbe7963149976bba8cdf6f3cca7943
Thang Vo <thanhthang3375@gmail.com>
2024-12-12 09:55:46 +0700

    [update] remove MACHINE_LIST hard code

--- projects/move_folder.py
+++ projects/move_folder.py
@@ -7,58 +7,14 @@ today = datetime.datetime.now()
 DROPBOX_PATH = r"D:\FlashPOD Dropbox"
 BACKUP_PATH = DROPBOX_PATH + r"\BackupFlashPOD"
 FLASHPOD_PATH = DROPBOX_PATH + r"\FlashPOD"
+FOLDER_DIR = str(today.year) + "_" + str(today.month)
 
 # ---------------Nhập tên folder muốn chuyển----------------------
 FOLDER_NAME = "2024_12_7"
 # ----------------------------------------------------------------
 
-FOLDER_DIR = str(today.year) + "_" + str(today.month)
-MACHINE_LIST = [
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10,
-    11,
-    12,
-    13,
-    14,
-    15,
-    16,
-    17,
-    18,
-    19,
-    20,
-    21,
-    22,
-    23,
-    24,
-    25,
-    26,
-    27,
-    28,
-    29,
-    30,
-    31,
-    32,
-    33,
-    34,
-    35,
-    36,
-    37,
-    38,
-    39,
-    40,
-    41,
-    42,
-]
-
-for machine in MACHINE_LIST:
+for machine in range(1, 43):  # Machine 1 to 42
+    # print(machine)
     machine_name = "Machine " + str(machine)
     src_path = os.path.join(FLASHPOD_PATH, machine_name, FOLDER_DIR)
     des_path = os.path.join(BACKUP_PATH, machine_name)
